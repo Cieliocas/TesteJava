cda1c88e8ad995f25e47f71909c3beffceeced3a
cieliocas <francielioevangelista@hotmail.com>
2025-03-27 19:03:15 -0300

    update Main class to include data type size information in main method

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        byte => 8 bits => -128 to 127
+        short => 16 bits => -32,768 to 32,767
+        int => 32 bits => -2,147,483,648 to 2,147,483,647
+        long => 64 bits => -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
     }
 }
